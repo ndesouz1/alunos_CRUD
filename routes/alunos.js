2139f158bbc9c8a90f9db4fdb1c26c72b56b32ce
ndesouz1 <eng.nayara@email.com>
2021-01-30 01:08:01 -0300

    feature/POST

--- routes/alunos.js
+++ routes/alunos.js
@@ -23,4 +23,12 @@ router.get('/', (req, res) =>  {
     res.send(alunos);
 });
 
+router.post('/', (req, res) =>  {
+    const aluno = req.body;
+
+    alunos.push(aluno);
+
+    res.send('Student with the name ${sluno.firstName} added to the database!');
+});
+
 export default router; 
